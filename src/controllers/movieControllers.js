972f13610e16d8a62e1a1e2f14b63b17bbcae807
Gaël <prigentgael@gmail.com>
2024-05-11 17:45:03 +0200

    Connected with database

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -1,32 +1,17 @@
-const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    color: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    color: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
+const movies = [/* ... */];
+
+const database = require("../../database");
 
 const getMovies = (req, res) => {
-  res.json(movies);
+  database
+    .query("select * from movies")
+    .then(([movies]) => {
+      res.json(movies); // use res.json instead of console.log
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
 
 const getMovieById = (req, res) => {
