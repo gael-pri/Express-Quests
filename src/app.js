348cc9b9379d1ebe13189cc67bf5f74d24390f6c
Gaël <prigentgael@gmail.com>
2024-05-12 10:48:45 +0200

    Méthode DELETE et suppression de données

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const validateMovie = require("./middlewares/validateMovie");
 app.post("/api/movies", validateMovie, movieControllers.postMovie);
 
 app.put("/api/movies/:id", movieControllers.updateMovie);
+app.delete("/api/movies/:id", movieControllers.deleteMovie);
 
 const userControllers = require("./controllers/userControllers");
 app.get("/api/users", userControllers.getUsers);
@@ -19,5 +20,6 @@ const validateUser = require("./middlewares/validateUser");
 app.post("/api/users", validateUser, userControllers.postUser);
 
 app.put("/api/users/:id", userControllers.updateUser);
+app.delete("/api/users/:id", userControllers.deleteUser);
 
 module.exports = app;
