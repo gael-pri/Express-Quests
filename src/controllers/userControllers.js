82ceb35183d044e7583f585729a2f010ab184f8b
Gaël <prigentgael@gmail.com>
2024-05-12 07:45:25 +0200

    Express 01.2bis - GET en détail

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -3,14 +3,39 @@ const movies = [/* ... */];
 const database = require("../../database");
 
 const getUsers = (req, res) => {
+  const initialSql = "select * from users";
+  const where = [];
+
+  if (req.query.language != null) {
+    where.push({
+      column: "language",
+      value: req.query.language,
+      operator: "=",
+    });
+  }
+  if (req.query.city != null) {
+    where.push({
+      column: "city",
+      value: req.query.city,
+      operator: "=",
+    });
+  }
+
   database
-    .query("select * from users")
+    .query(
+      where.reduce(
+        (sql, { column, operator }, index) =>
+          `${sql} ${index === 0 ? "where" : "and"} ${column} ${operator} ?`,
+        initialSql
+      ),
+      where.map(({ value }) => value)
+    )
     .then(([users]) => {
-      res.json(users); // use res.json instead of console.log
+      res.json(users);
     })
     .catch((err) => {
       console.error(err);
-      res.sendStatus(500);
+      res.status(500).send("Error retrieving data from database");
     });
 };
 
